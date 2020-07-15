3ebeb21c3a639a3a67277605fccf16f3dbc4ec3c
josunaFC <38095767+josunaFC@users.noreply.github.com>
2020-07-15 10:49:05 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,20 +4,22 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     Hero.change(LedSpriteProperty.X, 1)
 })
+// Based on this tutorial:drive.google.com/file/d/1p1fm9SlFVr8NQ9ctQvhZ7H-okfxXhabW/view
 let Rock: game.LedSprite = null
 let Hero: game.LedSprite = null
 Hero = game.createSprite(2, 4)
+game.startCountdown(60000)
 basic.forever(function () {
-    basic.pause(randint(3000, 5000))
+    basic.pause(randint(1000, 2000))
     Rock = game.createSprite(randint(0, 4), 0)
     for (let index = 0; index < 4; index++) {
-        basic.pause(200)
+        basic.pause(150)
         Rock.change(LedSpriteProperty.Y, 1)
     }
     if (Hero.isTouching(Rock)) {
         game.gameOver()
     }
-    basic.pause(200)
+    basic.pause(150)
     game.addScore(1)
     Rock.delete()
 })
